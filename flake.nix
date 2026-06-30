2b35aaf17586573c77b561a18fbc0b1a7b002d89
SkadiTheWolf <yawaluke6@gmail.com>
2026-06-30 17:27:20 +0200

    nixvim follows

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nixvim.url = "github:nix-community/nixvim/nixos-26.05";
-    #nixvim.inputs.nixpkgs.follows = "nixpkgs";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ {
