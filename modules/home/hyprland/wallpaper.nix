46177b8f23d60cd8db8574327fcecf5d5103bc19
SkadiTheWolf <yawaluke6@gmail.com>
2026-07-15 11:01:34 +0200

    forgot another closing bracket

--- modules/home/hyprland/wallpaper.nix
+++ modules/home/hyprland/wallpaper.nix
@@ -7,35 +7,36 @@ in {
 
   xdg.configFile."hypr/hyprpaper.conf".text = ''
 
-        wallpaper {
-            monitor = DP-1
-            path = ~/.config/dotfiles/assets/wallpapers/main.jpg
-            fit_mode = cover
-        }
+    wallpaper {
+        monitor = DP-1
+        path = ~/.config/dotfiles/assets/wallpapers/main.jpg
+        fit_mode = cover
+    }
 
-        wallpaper {
-            monitor = HDMI-A-1
-            path = ~/.config/dotfiles/assets/wallpapers/main.jpg
-            fit_mode = cover
-        }
+    wallpaper {
+        monitor = HDMI-A-1
+        path = ~/.config/dotfiles/assets/wallpapers/main.jpg
+        fit_mode = cover
+    }
 
-        wallpaper {
-            monitor = DVI-D-1
-            path = ~/.config/dotfiles/assets/wallpapers/main.jpg
-            fit_mode = cover
-        }
+    wallpaper {
+        monitor = DVI-D-1
+        path = ~/.config/dotfiles/assets/wallpapers/main.jpg
+        fit_mode = cover
+    }
 
-        wallpaper {
-          monitor = DP-2
-          path = ~/.config/dotfiles/assets/wallpapers/main.jpg
-         fit_mode = cover
+    wallpaper {
+      monitor = DP-2
+      path = ~/.config/dotfiles/assets/wallpapers/main.jpg
+     fit_mode = cover
     }
 
 
-        wallpaper {
-          monitor =
-          path = ~/.config/dotfiles/assets/wallpapers/main.jpg
-          fit_mode = cover
+    wallpaper {
+      monitor =
+      path = ~/.config/dotfiles/assets/wallpapers/main.jpg
+      fit_mode = cover
+    }
 
   '';
 }
